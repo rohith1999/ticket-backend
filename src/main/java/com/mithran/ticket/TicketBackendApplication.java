dae6e2bb116f9f58563d10d935fb00fc1c77b8cc
rohith1999 <rohithsuri3@gmail.com>
2022-08-20 11:57:17 +0530

    first commit

---
+++ src/main/java/com/mithran/ticket/TicketBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mithran.ticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mithran/ticket/TicketBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mithran.ticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
